585b6ab9bc87183d9cb52844d97ef1f9c084e87f
Emmanuel inambao <inambaocatherine6@gmail.com>
2026-09-25 16:52:34 +0200

    Compose Blaze and Nyx animation atlases

---
+++ components/sprite-data/atlases.ts
@@ -0,0 +1,21 @@
+import b0 from "./blaze0";
+import b1 from "./blaze1";
+import b2 from "./blaze2";
+import b3 from "./blaze3";
+import b4 from "./blaze4";
+import b5 from "./blaze5";
+import b6 from "./blaze6";
+import b7 from "./blaze7";
+import n0 from "./nyx0";
+import n1 from "./nyx1";
+import n2 from "./nyx2";
+import n3 from "./nyx3";
+import n4 from "./nyx4";
+import n5 from "./nyx5";
+import n6 from "./nyx6";
+
+export const BLAZE_ATLAS =
+  "data:image/webp;base64," + b0 + b1 + b2 + b3 + b4 + b5 + b6 + b7;
+
+export const NYX_ATLAS =
+  "data:image/webp;base64," + n0 + n1 + n2 + n3 + n4 + n5 + n6;
